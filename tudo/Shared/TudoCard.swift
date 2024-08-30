5a5ae015efb30eb34fda55940387f97898176260
Bholanath <bholanathbarik9748@gmail.com>
2024-08-30 19:54:36 +0530

    Feat: projectinit

---
+++ tudo/Shared/TudoCard.swift
@@ -0,0 +1,18 @@
+//
+//  TudoCard.swift
+//  tudo
+//
+//  Created by Bholanath Barik on 29/08/24.
+//
+
+import SwiftUI
+
+struct TudoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TudoCard()
+}
